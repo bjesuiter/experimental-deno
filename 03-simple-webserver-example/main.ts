4d1d36c3520e1087e6d030e4bb5009c58c3cce52
Benjamin  Jesuiter <bjesuiter@Zephir-MBP-16.local>
2020-06-04 12:12:30 +0200

    fix version of deno http serv

--- 03-simple-webserver-example/main.ts
+++ 03-simple-webserver-example/main.ts
@@ -1,6 +1,6 @@
 // run with 
 // deno run --allow-net 03-simple-webserver-example/main.ts 
-import { serve } from 'https://deno.land/std/http/server.ts';
+import { serve } from 'https://deno.land/std@v0.55.0/http/server.ts';
 const s = serve({ port: 8000 })
 console.log('http://localhost:8000/')
 for await (const req of s) {
